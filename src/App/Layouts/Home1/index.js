2e009a0c9cec950ab8d8cbf1ba89104d8e409030
Ramyalshurafa <ramyshurafa@hotmail.com>
2018-10-25 02:32:52 +0300

    creating Home1 layout

---
+++ src/App/Layouts/Home1/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  NavLink
+} from "react-router-dom";
+
+import Slider from "../../CommonComponents/Slider";
+
+class Home1 extends Component {
+
+  render() {
+    return (
+        <Slider />
+    )
+  }
+}
+
+export default Home1;
