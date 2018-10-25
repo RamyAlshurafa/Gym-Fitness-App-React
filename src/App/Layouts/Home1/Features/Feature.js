3e8d08383d8b7c63de7fa08820ae9cd0ecee5396
Ramyalshurafa <ramyshurafa@hotmail.com>
2018-10-26 00:43:35 +0300

    ading prop-types validation for feature component

--- src/App/Layouts/Home1/Features/Feature.js
+++ src/App/Layouts/Home1/Features/Feature.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Feature = (props) => {
   const {
@@ -21,6 +22,12 @@ const Feature = (props) => {
       </button>
     </div>
   )
+}
+
+Feature.propTypes = {
+  imgUrl: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired
 }
 
 export default Feature;
