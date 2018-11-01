e89d063da5ae0aa04f59ddccd5feff567b64ffe0
Ramyalshurafa <ramyshurafa@hotmail.com>
2018-11-01 23:51:10 +0200

    make lightbox component to get images array in its props

--- src/App/CommonComponents/LightBox/index.js
+++ src/App/CommonComponents/LightBox/index.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
 class LightBox extends Component {
+  state = {
+    images: []
+  }
+
+  componentDidMount() {
+    this.setState({
+      ...this.state,
+      images: this.props.images
+    })
+  }
+
   render() {
     return (
       <div className="lightbox">
