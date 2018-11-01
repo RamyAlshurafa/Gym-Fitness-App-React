97fead6580ff58ecd39edfd5dcda604b8fadab79
Ramyalshurafa <ramyshurafa@hotmail.com>
2018-11-01 17:00:11 +0200

    first commit on lightbox component

---
+++ src/App/CommonComponents/LightBox/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class LightBox extends Component {
+  render() {
+    return (
+      <div className="lightbox">
+      </div>
+    )
+  }
+}
+
+export default LightBox;
