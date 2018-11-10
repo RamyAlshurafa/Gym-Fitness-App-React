4316faa72c98eef986062b05a887465d8f492186
Ramyalshurafa <ramyshurafa@hotmail.com>
2018-11-10 22:47:34 +0200

    change data images with real imageUrl

--- src/App/Layouts/Home1/WorkoutClasses/data.js
+++ src/App/Layouts/Home1/WorkoutClasses/data.js
@@ -12,19 +12,19 @@ export const data = [
     categoryIndex: 1
   },
   {
-    imageUrl: "http://clubparqueverde.es/wp-content/uploads/2017/03/mancuernas_gym.jpg",
+    imageUrl: "http://sitetemplate.demo.ithemeslab.com/befit/male/assets/images/instagram/big5.jpg",
     title: "Gym3",
     description: "gym disc 3",
     categoryIndex: 2
   },
   {
-    imageUrl: "http://clubparqueverde.es/wp-content/uploads/2017/03/mancuernas_gym.jpg",
+    imageUrl: "http://sitetemplate.demo.ithemeslab.com/befit/male/assets/images/instagram/big4.jpg",
     title: "Gym4",
     description: "gym disc 4",
     categoryIndex: 3
   },
   {
-    imageUrl: "http://clubparqueverde.es/wp-content/uploads/2017/03/mancuernas_gym.jpg",
+    imageUrl: "http://sitetemplate.demo.ithemeslab.com/befit/male/assets/images/instagram/big2.jpg",
     title: "Gym5",
     description: "gym disc 5",
     categoryIndex: 4
