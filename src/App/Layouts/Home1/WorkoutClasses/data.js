9fb9ce3ba16e8b29aa081b9b27ecf24fe1ccfb07
Ramyalshurafa <ramyshurafa@hotmail.com>
2018-11-10 15:09:50 +0200

    change imgUrl key into imageUrl

--- src/App/Layouts/Home1/WorkoutClasses/data.js
+++ src/App/Layouts/Home1/WorkoutClasses/data.js
@@ -1,30 +1,30 @@
 export const data = [
   {
-    imgUrl: "http://clubparqueverde.es/wp-content/uploads/2017/03/mancuernas_gym.jpg",
+    imageUrl: "http://clubparqueverde.es/wp-content/uploads/2017/03/mancuernas_gym.jpg",
     title: "Gym1",
     description: "gym disc 1",
     categoryIndex: 0
   },
   {
-    imgUrl: "https://www.dailymedicalinfo.com/wp-content/uploads/2011/09/maxresdefault-1.jpg",
+    imageUrl: "https://www.dailymedicalinfo.com/wp-content/uploads/2011/09/maxresdefault-1.jpg",
     title: "Gym2",
     description: "gym disc 2",
     categoryIndex: 1
   },
   {
-    imgUrl: "http://clubparqueverde.es/wp-content/uploads/2017/03/mancuernas_gym.jpg",
+    imageUrl: "http://clubparqueverde.es/wp-content/uploads/2017/03/mancuernas_gym.jpg",
     title: "Gym3",
     description: "gym disc 3",
     categoryIndex: 2
   },
   {
-    imgUrl: "http://clubparqueverde.es/wp-content/uploads/2017/03/mancuernas_gym.jpg",
+    imageUrl: "http://clubparqueverde.es/wp-content/uploads/2017/03/mancuernas_gym.jpg",
     title: "Gym4",
     description: "gym disc 4",
     categoryIndex: 3
   },
   {
-    imgUrl: "http://clubparqueverde.es/wp-content/uploads/2017/03/mancuernas_gym.jpg",
+    imageUrl: "http://clubparqueverde.es/wp-content/uploads/2017/03/mancuernas_gym.jpg",
     title: "Gym5",
     description: "gym disc 5",
     categoryIndex: 4
